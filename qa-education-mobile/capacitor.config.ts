9dad1a9123d03690d7f4d8542bd38545f3564e99
ARTSOFTE\tsychev <t.sychev@artsofte.ru>
2024-02-15 15:38:40 +0500

    1

---
+++ qa-education-mobile/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qa-education-mobile',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
